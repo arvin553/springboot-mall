f2e02349e4136987b1bb65be38ce4dac06fc8a77
arvin553 <94210688+arvin553@users.noreply.github.com>
2024-05-21 17:32:51 +0800

    商品功能 - 查詢商品列表分頁

--- springboot-mall/src/main/java/com/arvin/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/arvin/springbootmall/dao/impl/ProductDaoImpl.java
@@ -31,17 +31,26 @@ public class ProductDaoImpl implements ProductDao {
                 "FROM product WHERE 1=1";  //用於拼接下面的sql語句
         Map<String,Object> map = new HashMap<>();
 
+        //查詢條件
         if(productQueryParams.getCategory() != null){
             sql += " AND category=:category";
             map.put("category", productQueryParams.getCategory().name());
         }
 
+        //排序
         if(productQueryParams.getSearch() != null){
             sql += " AND product_name LIKE :search";
             map.put("search","%" + productQueryParams.getSearch() + "%");
         }
 
+        //分頁
         sql=sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        sql=sql+" LIMIT :limit OFFSET :offset";
+        map.put("limit",productQueryParams.getLimit());
+        map.put("offset",productQueryParams.getOffset());
+
+
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
 
         return productList;
