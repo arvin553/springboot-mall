e7d72bfec175b5573a87ee33b3403967df7db48f
arvin553 <94210688+arvin553@users.noreply.github.com>
2024-05-21 18:24:13 +0800

    商品功能 - 查詢商品列表分頁+總筆數

--- springboot-mall/src/main/java/com/arvin/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/arvin/springbootmall/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.arvin.springbootmall.dto.ProductQueryParams;
 import com.arvin.springbootmall.dto.ProductRequest;
 import com.arvin.springbootmall.model.Product;
 import com.arvin.springbootmall.service.ProductService;
+import com.arvin.springbootmall.util.Page;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
@@ -24,7 +25,7 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getProducts(
+    public ResponseEntity<Page<Product>> getProducts(
             //查詢條件
             @RequestParam(required = false) ProductCategory category,
             @RequestParam(required = false) String search,
@@ -45,9 +46,20 @@ public class ProductController {
         productQueryParams.setLimit(limit);
         productQueryParams.setOffset(offset);
 
+        //取得商品列表
         List<Product> productList =productService.getProducts(productQueryParams);
 
-        return ResponseEntity.status(HttpStatus.OK).body(productList);
+        //取得商品總數
+        Integer total =productService.countProduct(productQueryParams);
+
+        // 設定分頁
+        Page<Product> page = new Page<>();
+        page.setLimit(limit);
+        page.setOffset(offset);
+        page.setTotal(total);
+        page.setResults(productList);
+
+        return ResponseEntity.status(HttpStatus.OK).body(page);
 
     }
 
